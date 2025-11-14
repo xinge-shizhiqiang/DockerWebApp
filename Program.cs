968664469538dc170d1f8adef64bf01e5cb04252
xinge-shizhiqiang <912034159@qq.com>
2025-11-14 11:42:22 +0800

    111

--- Program.cs
+++ Program.cs
@@ -18,11 +18,14 @@ namespace DockerWebApp
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
