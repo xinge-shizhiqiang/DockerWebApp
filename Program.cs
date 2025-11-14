3b4dad2b45a2f99bf86af054cd2aafc431b059c3
xinge-shizhiqiang <912034159@qq.com>
2025-11-14 11:34:25 +0800

    builder.WebHost.UseUrls("http://0.0.0.0:80");

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace DockerWebApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.WebHost.UseUrls("http://0.0.0.0:80");
 
             // Add services to the container.
 
